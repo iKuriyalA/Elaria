2a7cdc123c566efc1e9cd8cf0ea4aa2447f1ec84
sujal-bizoara <sujalbizoara22@gmail.com>
2025-12-29 22:17:41 -0500

    Update data_structures.py
    .....

--- data_structures.py
+++ data_structures.py
@@ -1,14 +1,8 @@
-"""
-data_structures.py
-------------------
-All mutable state lives here.
-Each structure exposes a common interface:
-- execute_command(action, args)
-"""
+# Mutable data structures
 
-# --------------------------------------------------
-# ARRAY
-# --------------------------------------------------
+from algorithms import list_diagram_display
+
+
 class Array:
     def __init__(self):
         self.data = []
@@ -17,50 +11,40 @@ class Array:
         if action == "ADD":
             self.data.extend(args)
         elif action == "REMOVE":
-            for val in args:
-                if val in self.data:
-                    self.data.remove(val)
-        elif action == "DISPLAY":
-            print(self.data)
+            for v in args:
+                if v in self.data:
+                    self.data.remove(v)
+        elif action == "DIAGRAM":
+            list_diagram_display(self.data)
         else:
-            print("Unknown ARRAY command.")
+            print("Unknown ARRAY command")
 
 
-# --------------------------------------------------
-# STACK (LIFO)
-# --------------------------------------------------
 class Stack:
     def __init__(self):
         self.data = []
 
     def execute_command(self, action, args):
         if action == "PUSH":
-            for val in args:
-                self.data.append(val)
-        elif action == "POP":
-            if self.data:
-                print("Popped:", self.data.pop())
-        elif action == "DISPLAY":
-            print(self.data)
+            self.data.extend(args)
+        elif action == "POP" and self.data:
+            print("Popped:", self.data.pop())
+        elif action == "DIAGRAM":
+            list_diagram_display(self.data)
         else:
-            print("Unknown STACK command.")
+            print("Unknown STACK command")
 
 
-# --------------------------------------------------
-# QUEUE (FIFO)
-# --------------------------------------------------
 class Queue:
     def __init__(self):
         self.data = []
 
     def execute_command(self, action, args):
         if action == "ENQUEUE":
-            for val in args:
-                self.data.append(val)
-        elif action == "DEQUEUE":
-            if self.data:
-                print("Dequeued:", self.data.pop(0))
-        elif action == "DISPLAY":
-            print(self.data)
+            self.data.extend(args)
+        elif action == "DEQUEUE" and self.data:
+            print("Dequeued:", self.data.pop(0))
+        elif action == "DIAGRAM":
+            list_diagram_display(self.data)
         else:
-            print("Unknown QUEUE command.")
+            print("Unknown QUEUE command")
